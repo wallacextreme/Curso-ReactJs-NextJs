feef0f108e6748fa79e1985d98ede50f5732c842
Wallacextreme <Wallacextreme@hotmail.com>
2022-07-29 09:55:14 -0300

    Fiz alguns testes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,20 +1,19 @@
 import './styles.css';
 
-import { Component } from "react";
+import { Component } from 'react';
 
 export class Button extends Component {
-    render() {
-        const { text, onClick, disabled } = this.props;
+  render() {
+    const { text, onClick, disabled } = this.props;
 
-
-        return (
-         <button 
-         className='button' 
-         onClick={onClick}
-         disabled={disabled}
-        >
-           {text}
-        </button>
-        );
-    }
+    return (
+      <button 
+        className='button' 
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/global-styles.css';
 
 import { Home } from './templates/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
